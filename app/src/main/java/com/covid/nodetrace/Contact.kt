1024a557f1963b37a7f604c2faf87a2ea532057b
NiekBijman <niekbijman@gmail.com>
2020-09-27 22:46:47 +0200

    NODE-16 Update database entry structure

--- app/src/main/java/com/covid/nodetrace/Contact.kt
+++ app/src/main/java/com/covid/nodetrace/Contact.kt
@@ -5,34 +5,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "contacts")
-class Contact {
-    @PrimaryKey val ID : String
-    @ColumnInfo(name = "date") val date : Long
-    @ColumnInfo(name = "latitude") var latitude : Double = -1.0
-    @ColumnInfo(name = "longitude") var longitude : Double = -1.0
-    @ColumnInfo(name = "duration") var duration : Long = -1
-    @ColumnInfo(name = "distance") var distance : Double = -1.0
-
-    constructor(contactID : String, contactDate : Long) {
-        ID = contactID
-        date = contactDate
-    }
-
-    constructor(contactID : String, contactDate : Long, lat : Double ,long : Double) {
-        ID = contactID
-        date = contactDate
-        latitude = lat
-        longitude = long
-    }
-
-    constructor(contactID : String, contactDate : Long, lat : Double ,long : Double , contactDuration : Long, contactDistance : Double ) {
-        ID = contactID
-        date = contactDate
-        latitude = lat
-        longitude = long
-        duration = contactDuration
-        distance = contactDistance
-    }
+@Entity
+open class Contact (
+    @PrimaryKey open val ID : String,
+    @ColumnInfo(name = "date") open val date : Long,
+    @ColumnInfo(name = "latitude") open var latitude : Double = -1.0,
+    @ColumnInfo(name = "longitude") open var longitude : Double = -1.0,
+    @ColumnInfo(name = "duration") open var duration : Long = -1,
+    @ColumnInfo(name = "distance") open var distance : Double = -1.0) {
 
 }
