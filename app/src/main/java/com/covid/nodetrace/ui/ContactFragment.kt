e08c515c00eeee76b346cb2c243398718ae15762
NiekBijman <niekbijman@gmail.com>
2020-09-27 15:25:05 +0200

    NODE-16 Update location configuration

--- app/src/main/java/com/covid/nodetrace/ui/ContactFragment.kt
+++ app/src/main/java/com/covid/nodetrace/ui/ContactFragment.kt
@@ -8,17 +8,29 @@ import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.covid.nodetrace.ContactHistoryAdapter
 import com.covid.nodetrace.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 /**
  * The contact screen of the app indicates all the contacts that the user has had with people that
  * were in the same vicinity and time period as them. It contains a list of encounters the user has had in the past
  */
-class ContactFragment : Fragment() {
+class ContactFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var contactHistoryListView : ListView
     private lateinit var contactHistoryAdapter : ContactHistoryAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.contact_screen, container, false)
     }
@@ -30,5 +42,26 @@ class ContactFragment : Fragment() {
         contactHistoryAdapter = ContactHistoryAdapter(requireActivity())
         contactHistoryListView.adapter = contactHistoryAdapter
         requireActivity().registerForContextMenu(contactHistoryListView)
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Stockholm and move the camera
+        val stockholm = LatLng(59.0, 18.0)
+        mMap.addMarker(MarkerOptions()
+            .position(stockholm)
+            .title("Marker in Stockholm"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm))
     }
 }
