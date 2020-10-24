98d50d341b5902e7390b226102da1dbdd84f6593
NiekBijman <niekbijman@gmail.com>
2020-10-24 13:57:00 +0200

    NODE-25 Add permission check wrapper when accessing Bluetooth object

--- app/src/main/java/com/covid/nodetrace/Bluetooth.kt
+++ app/src/main/java/com/covid/nodetrace/Bluetooth.kt
@@ -1,6 +1,9 @@
 package com.covid.nodetrace
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.messages.MessagesClient
 import com.google.android.gms.nearby.messages.MessagesOptions
@@ -10,9 +13,14 @@ import com.google.android.gms.nearby.messages.NearbyPermissions
 object Bluetooth {
 
     public fun getBluetoothAccess(context: Context) : MessagesClient? {
-        return Nearby.getMessagesClient(context, MessagesOptions.Builder()
-                .setPermissions(NearbyPermissions.BLE)
-                .build()
-        )
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                return Nearby.getMessagesClient(context, MessagesOptions.Builder()
+                        .setPermissions(NearbyPermissions.BLE)
+                        .build()
+                )
+        }
+        else {
+            return null
+        }
     }
 }
